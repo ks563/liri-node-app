23e1ff3c99a05632b4b5ba4fac3a081ae252f0b3
ks563 <karen.shea90@gmail.com>
2019-02-14 18:39:29 -0600

    added npm packages

---
+++ liri.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+function Liri(concert, spotify, movie, doWhat) {
+    
+}
+
+
+
+// var song = function () {
+    
+
+
+//     var keys = require("./keys.js");
+
+//     var spotify = new Spotify(keys.spotify);
+
+// };
